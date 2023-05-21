4c496eee09c184f7e88709e6713da8a7fbfd1c1e
Phil <PhilboBaggins@users.noreply.github.com>
2023-05-21 21:28:07 +1000

    Clone repo

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
+use std::process::Command;
 use dialoguer::{theme::ColorfulTheme, FuzzySelect};
 
 mod github;
 use github::fetch_repo_list;
 
 fn main() -> Result<(), ureq::Error> {
+
+    // TODO: Ask user for Github username ... provide list of previously selected usernames and allow them to enter a new one if desired
     let username = "PhilboBaggins";
 
     println!("Fetching list of repositories for {}", username);
@@ -16,7 +19,20 @@ fn main() -> Result<(), ureq::Error> {
         .interact()
         .unwrap();
 
-    println!("TODO: git clone {}", clone_urls[selection_index]);
+    let name = &names[selection_index];
+    let clone_url = &clone_urls[selection_index];
+
+    // TODO: Ask user for local directory name... default to `name`
+    let local_dir = format!("{}.git", name);
+
+    Command::new("git")
+        .arg("clone")
+        .arg(clone_url)
+        .arg(local_dir)
+        .spawn()
+        .expect("failed to execute git clone")
+        .wait()
+        .expect("failed to wait on child");
 
     Ok(())
 }
