57e028ee059f96e6d8c1fd47ab7f709981a5fe09
Phil <PhilboBaggins@users.noreply.github.com>
2023-05-21 23:06:23 +1000

    Start ask_for_username()

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,12 @@ mod github;
 use github::fetch_repo_list;
 
 fn main() -> Result<(), ureq::Error> {
-
-    // TODO: Ask user for Github username ... provide list of previously selected usernames and allow them to enter a new one if desired
-    let username = "PhilboBaggins";
+    // Ask user for Github username 
+    let username = ask_for_username()?;
 
     // Fetch list of user's repositories
     println!("Fetching list of repositories for {}...", username);
-    let (names, clone_urls) = fetch_repo_list(username)?;
+    let (names, clone_urls) = fetch_repo_list(&username)?;
 
     // Select repository
     let selection_index = FuzzySelect::with_theme(&ColorfulTheme::default())
@@ -38,4 +37,33 @@ fn main() -> Result<(), ureq::Error> {
         .wait()?;
 
     Ok(())
+}
+
+fn ask_for_username() -> Result<String, std::io::Error> {
+    // TODO: Read list of previous usernames
+    let prev_usernames = vec!["PhilboBaggins"];
+
+    let username = match prev_usernames.len() {
+        0 => {
+            Input::with_theme(&ColorfulTheme::default())
+                .with_prompt("Github username")
+                .interact_text()?
+                //.unwrap()
+        },
+        1 => {
+            let prev_username = *prev_usernames.first().unwrap();
+            Input::with_theme(&ColorfulTheme::default())
+                .with_prompt("Github username")
+                .default(prev_username.to_string())
+                .interact_text()?
+        },
+        _ => {
+            // TODO: Present list of previous usernames and allow user select one or enter a new one
+            prev_usernames.first().unwrap().to_string()
+        },
+    };
+    
+    // TODO: If `username` not in `prev_usernames` list; add it and save to config file
+    
+    Ok(username)
 }
