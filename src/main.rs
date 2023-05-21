2a8d399d4b181cfe463ec7d8e5b5c0f39f00e8ed
Phil <PhilboBaggins@users.noreply.github.com>
2023-05-21 21:47:10 +1000

    Comments and println

--- src/main.rs
+++ src/main.rs
@@ -9,16 +9,17 @@ fn main() -> Result<(), ureq::Error> {
     // TODO: Ask user for Github username ... provide list of previously selected usernames and allow them to enter a new one if desired
     let username = "PhilboBaggins";
 
-    println!("Fetching list of repositories for {}", username);
+    // Fetch list of user's repositories
+    println!("Fetching list of repositories for {}...", username);
     let (names, clone_urls) = fetch_repo_list(username)?;
 
+    // Select repository
     let selection_index = FuzzySelect::with_theme(&ColorfulTheme::default())
         .with_prompt("Select repository")
         .default(0)
         .items(&names[..])
         .interact()
         .unwrap();
-
     let name = &names[selection_index];
     let clone_url = &clone_urls[selection_index];
 
@@ -29,14 +30,14 @@ fn main() -> Result<(), ureq::Error> {
         .interact_text()
         .unwrap();
 
+    // Git clone
     Command::new("git")
         .arg("clone")
         .arg(clone_url)
         .arg(local_dir)
         .spawn()
         .expect("failed to execute git clone")
-        .wait()
-        .expect("failed to wait on child");
+        .wait()?;
 
     Ok(())
 }
